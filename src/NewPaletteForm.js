015c41d210e28cfc517e007cc8b148aabfffbbcf
Mike Paiva <mikepaiva@resnet300-passy-ip-10-36-33.153.resnet.yorku.ca>
2020-08-18 01:36:20 -0400

    link to newpaletteform page

---
+++ src/NewPaletteForm.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { render } from "@testing-library/react";
+
+class NewPaletteForm extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
+
+export default NewPaletteForm;
